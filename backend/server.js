6698750ccdc6cd7f78d7a9111eb495f6045c8fb1
KhetwalDevesh <deveshkhetwal05@gmail.com>
2023-02-06 12:35:52 +0530

    added res header

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const router = require('./routes/product.route.js');
 
 app.use(express.json());
 app.use(cors());
+res.header( "Access-Control-Allow-Origin" );
 // app.use('/api/products',router);
 app.get('/ping',()=>{
     res.send("pong");
