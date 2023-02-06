50035877eae74667aa8a9db77aec2cdfad0fccc1
KhetwalDevesh <deveshkhetwal05@gmail.com>
2023-02-06 12:19:39 +0530

    m

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const router = require('./routes/product.route.js');
 
 app.use(express.json());
 app.use(cors());
-app.use('/api/products',router);
+// app.use('/api/products',router);
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on port no. ${process.env.PORT}`);
