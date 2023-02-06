aa62ffa05b4404645544d790437df746306c68c0
KhetwalDevesh <deveshkhetwal05@gmail.com>
2023-02-06 12:21:14 +0530

    testing ping

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const router = require('./routes/product.route.js');
 app.use(express.json());
 app.use(cors());
 // app.use('/api/products',router);
+app.get('/ping',()=>{
+    res.send("pong");
+})
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on port no. ${process.env.PORT}`);
