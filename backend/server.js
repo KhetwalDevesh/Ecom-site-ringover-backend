245a0727905f42d4eb72bcb9158a9eaaff3ae33d
KhetwalDevesh <deveshkhetwal05@gmail.com>
2023-02-06 13:40:21 +0530

    ping-pong

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const cors = require('cors')
 // require('dotenv').config();
 const app = express();
-const router = require('./routes/product.route.js');
+// const router = require('./routes/product.route.js');
 
 app.use(express.json());
 app.use(cors());
 // res.header( "Access-Control-Allow-Origin" );
-app.use('/api/products',router);
+// app.use('/api/products',router);
 app.get('/ping',(req,res)=>{
     res.send("-pong");
 })
